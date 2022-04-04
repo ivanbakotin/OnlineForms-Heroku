a60b5f42ac6085fca956a7cbeb61985df790e1a6
ivan <ivan.bakotin1999@gmail.com>
2022-04-04 16:07:21 +0200

    check owner id and styling add question

---
+++ utils/groupArray.js
@@ -0,0 +1,15 @@
+module.exports = function groupArray(array, id) {
+  const map = {};
+
+  array.forEach((element) => {
+    const group = element[id];
+
+    if (!map[group]) {
+      map[group] = [];
+    }
+
+    map[group].push(element);
+  });
+
+  return map;
+};
